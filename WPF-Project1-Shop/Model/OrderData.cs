14dfc35d1d45dce0a8dd9fac4ca787f2c3b3a079
Lamlance <lamhoangdien113@gmail.com>
2023-04-10 23:32:53 +0700

    Add project files.

---
+++ WPF-Project1-Shop/Model/OrderData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Project1_Shop.Model
+{
+  public class OrderData
+  {
+    private string _name = "Product";
+    private DateTime _orderDate = DateTime.UtcNow;
+    private DateTime? _deliverDate = null;
+
+    public string Name { get => _name; set => _name = value; }
+    public DateTime OrderDate { get => _orderDate; set => _orderDate = value; }
+    public DateTime? DeliverDate { get => _deliverDate; set => _deliverDate = value; }
+  }
+}
---
+++ WPF-Project1-Shop/View/Orders.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_Project1_Shop.Model;
+
+namespace WPF_Project1_Shop
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    ObservableCollection<OrderData> orders = new ObservableCollection<OrderData>();
+    public MainWindow()
+    {
+      for(int i = 0;i < 40; i++)
+      {
+        orders.Add(new OrderData() { Name = $"Product#{i}" });
+      }
+      InitializeComponent();
+    }
+
+    private void ListOrderLoaded(object sender, RoutedEventArgs e)
+    {
+      this.ListOrder.ItemsSource = orders;
+    }
+  }
+}
