875e779a294205a679497d835e1958abc487261f
Lamlance <lamhoangdien113@gmail.com>
2023-04-14 22:01:02 +0700

    patches after merge

--- WPF-Project1-Shop/View/ProductsWindow.xaml.cs
+++ WPF-Project1-Shop/View/ProductsWindow.xaml.cs
@@ -29,25 +29,24 @@ namespace WPF_Project1_Shop.View
 
             // Set the DataContext of the view to itself (the Window)
             DataContext = this;
-            Products = new ObservableCollection<Product>
-            {
-            new Product { Id = 1, ImagePath = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
-            new Product { Id = 2, ImagePath = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
-            new Product { Id = 3, ImagePath = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
-            new Product { Id = 4, ImagePath = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
-            new Product { Id = 5, ImagePath = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
-            new Product { Id = 6, ImagePath = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
-            new Product { Id = 7, ImagePath = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
-            new Product { Id = 8, ImagePath = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
-            new Product { Id = 9, ImagePath = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
-            new Product { Id = 10, ImagePath = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
-            new Product { Id = 11, ImagePath = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
-            new Product { Id = 12, ImagePath = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
-            new Product { Id = 13, ImagePath = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
-            new Product { Id = 14, ImagePath = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
-            new Product { Id = 15, ImagePath = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
-
-            };
+            //Products = new ObservableCollection<Product>
+            //{
+            //new Product { Id = 1, Image = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
+            //new Product { Id = 2, Image = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
+            //new Product { Id = 3, Image = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
+            //new Product { Id = 4, Image = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
+            //new Product { Id = 5, Image = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png" ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
+            //new Product { Id = 6, Image = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png",  ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
+            //new Product { Id = 7, Image = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", , ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
+            //new Product { Id = 8, Image = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
+            //new Product { Id = 9, Image = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
+            //new Product { Id = 10, Image = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
+            //new Product { Id = 11, Image = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
+            //new Product { Id = 12, Image = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
+            //new Product { Id = 13, Image = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
+            //new Product { Id = 14, Image = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
+            //new Product { Id = 15, Image = "https://hanoicomputercdn.com/media/product/66888_hacom_macbook_pro_13_7.png", Brand = "APPLE", ProductName = "M2 - ABCDYXUS", Price = 99.99m, Quantity = 2500, },
+            //};
             ProductListView.ItemsSource = Products;
         }
 
