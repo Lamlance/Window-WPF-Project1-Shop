ee0d905531edfabc075c2242dad1ca753242f5db
hanphu <phuhanld@gmail.com>
2023-05-08 18:52:32 +0700

    edit style

---
+++ WPF-Project1-Shop/View/CategoryUserControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_Project1_Shop.ViewModel;
+
+namespace WPF_Project1_Shop.View
+{
+  /// <summary>
+  /// Interaction logic for CategoryUserControl.xaml
+  /// </summary>
+  public partial class CategoryUserControl : UserControl
+  {
+    static CategoryViewModel categoryViewModel = CategoryViewModel.NewInstance();
+    public CategoryUserControl()
+    {
+      InitializeComponent();
+    }
+
+    private void UserControlLoaded(object sender, RoutedEventArgs e)
+    {
+      //this.CategoryListView.ItemsSource = 
+    }
+  }
+}
