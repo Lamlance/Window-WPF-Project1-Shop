2d7befe6203e60ed8594e9037e7c71fcd556d3ed
Lamlance <lamhoangdien113@gmail.com>
2023-05-15 13:37:21 +0700

    Base repository

---
+++ WPF-Project1-Shop/EFCustomRepository/BaseCustomRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_Project1_Shop.EFModel;
+
+namespace WPF_Project1_Shop.EFCustomRepository
+{
+  public class BaseCustomRepository:IDisposable
+  {
+    RailwayContext context;
+
+    protected BaseCustomRepository(RailwayContext context)
+    {
+      this.context = context;
+    }
+
+    public RailwayContext dbContext { get => context; set => context = value; }
+
+    public void Dispose()
+    {
+      context.SaveChanges();
+      context.Dispose();
+    }
+  }
+}
